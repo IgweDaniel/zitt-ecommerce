e2a29429ffcb57aadad0d624f4d5d3c23a4bca9f
IgweDaniel <danielchibuzoigwe@gmail.com>
2020-06-07 23:30:21 +0100

    created next.config.js

---
+++ next.config.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+
+// const os = require("os");
+// const ifaces = os.networkInterfaces();
+// let address = null;
+// for (let prop in ifaces) {
+//   let iface = ifaces[prop].filter(function (details) {
+//     return details.family === "IPv4" && details.internal === false;
+//   });
+
+//   if (iface.length > 0) address = iface[0].address;
+// }
+
+module.exports = {
+  env: {
+    address,
+    contentfulSpaceId: process.env["CMS_ID"],
+    contentfulAccessToken: process.env["CMS_ACCESS_TOKEN"],
+  },
+};
